90a84bb2594492a5186596e17089d33d0cdf274c
Henrique Cordeiro <h.cordeirop@gmail.com>
2023-01-24 23:27:57 -0300

    refactor: improving views.py legibility and understanding

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import BookList
 from django.contrib import messages
+
 def index(request):
     if request.method == 'POST':
+        # The POST request returns a dictionary in which the second key is the button's name
         page = list(request.POST)[1]
         return redirect(f'{page}')
     return render(request, 'index.html')
@@ -10,10 +12,10 @@ def index(request):
 def create(request):
     if request.method == "POST":
         create_form = request.POST
-        title = create_form['title']
-        author = create_form['author']
-        year = create_form['year']
-        BookList.objects.create(title=title, author=author, year=year)
+        BookList.objects.create(
+            title=create_form['title'], 
+            author=create_form['author'], 
+            year=create_form['year'])
         return redirect('index')
     return render(request, 'create.html')
 
@@ -34,14 +36,13 @@ def update(request):
             updated_book.update(author=update_form['author'], year=update_form['year'])
             return redirect('index')
         else:
-            messages.add_message(request, messages.INFO, "This Book Doesn't exist.")
-
+            messages.add_message(request, messages.INFO, f'''The book "{title}" Doesn't exist.''')
     return render(request, 'update.html')
 
 def delete(request):
     if request.method == "POST":
-        title = request.POST['title']
-        deleted_book = BookList.objects.filter(title=title)
+        # title = request.POST['title']
+        deleted_book = BookList.objects.filter(title=request.POST['title'])
         if deleted_book:
             deleted_book.delete()
         return redirect('index')
